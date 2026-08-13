099350a2bca18cc4173829366c3230f68ffc9b75
WitherWeb <nikolaevr.web@gmail.com>
2026-08-13 04:53:46 +0300

    Частотный словарь слов

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-//Подсчёт чётных и нечётных
-
-const nums = [1, 2, 3, 4, 5, 6];
-
-const countEvenOdd = () => {
-  let even = 0
-  let add = 0
-    nums.forEach((el) => {
-        if(el % 2 === 1) {
-          even++
-        }else{
-          add++
-        }
-    })
-  return {even: even, add: add}
-}
-
-// Вызов:
-const result = countEvenOdd(nums);
-
-// Ожидаемый результат:
-const expected = { even: 3, odd: 3 };
-
-//Поиск минимума и максимума
-
 // Вход:
-const nums = [10, -3, 7, 0];
+const words = ['кот', 'пёс', 'кот', 'кот', 'пёс'];
 
-const minMax = (nums) => {
-  if(nums.lenght === 0) {
-    return {min: null, max: null}
-  }else {
-    return {min: Math.min(...nums), max: Math.max(...nums)}
-  }
- 
+const wordFreq = (words) => {
+  return words.reduce((obj, value) => {
+    if(!obj[value]){
+      obj[value] = 1
+    }else {
+      obj[value] = obj[value] + 1
+    }
+    
+    return obj
+  }, {})
 }
+
 // Вызов:
-const result = minMax(nums);
+const result = wordFreq(words);
 
 console.log(result)
 
 // Ожидаемый результат:
-const expected = { min: -3, max: 10 };
+const expected = { 'кот': 3, 'пёс': 2 };
