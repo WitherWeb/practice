4fa23b1690ea950d3d057dd9425c29cfead62267
WitherWeb <nikolaevr.web@gmail.com>
2026-08-13 04:15:10 +0300

    Поиск минимума и максимума

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Подсчёт чётных и нечётных
+
 const nums = [1, 2, 3, 4, 5, 6];
 
 const countEvenOdd = () => {
@@ -18,3 +20,24 @@ const result = countEvenOdd(nums);
 
 // Ожидаемый результат:
 const expected = { even: 3, odd: 3 };
+
+//Поиск минимума и максимума
+
+// Вход:
+const nums = [10, -3, 7, 0];
+
+const minMax = (nums) => {
+  if(nums.lenght === 0) {
+    return {min: null, max: null}
+  }else {
+    return {min: Math.min(...nums), max: Math.max(...nums)}
+  }
+ 
+}
+// Вызов:
+const result = minMax(nums);
+
+console.log(result)
+
+// Ожидаемый результат:
+const expected = { min: -3, max: 10 };
